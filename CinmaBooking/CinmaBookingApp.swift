4e9a828694d5a39baf27d76258e04ce8d894d051
Terry <Terryzxv@gmail.com>
2026-05-03 06:17:02 +1000

    Initial Commit

---
+++ CinmaBooking/CinmaBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinmaBookingApp.swift
+//  CinmaBooking
+//
+//  Created by Terry on 3/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct CinmaBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CinmaBooking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CinmaBooking
+//
+//  Created by Terry on 3/5/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
